4b611be2c2bc6cc148939f511d52aeabbc20f247
rainathecreatour-ops <rainathecreatour@gmail.com>
2026-02-17 06:24:37 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import './globals.css';
-
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style global jsx>{`
+        *, *::before, *::after {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
+        html, body {
+          height: 100%;
+          background: #fdf6f0;
+          font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+          color: #4a3728;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
